795ec25af79ba11b01e7b10a539b61724a30014f
Kelly Galakatos <kellygalakatos@mwc-024132.dhcp.missouri.edu>
2018-12-04 01:26:20 -0600

    Initial Commit

---
+++ Teleprompter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teleprompter
+//
+//  Created by Kelly Galakatos on 12/4/18.
+//  Copyright © 2018 Kelly Galakatos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
